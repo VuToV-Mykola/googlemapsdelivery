c1b48ceca76c76b1293b980f1d399be72fd853d3
VuToV-Mykola <61313070+VuToV-Mykola@users.noreply.github.com>
2022-01-01 17:59:13 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -65,14 +65,13 @@ function calcRoute() {
     `https://nominatim.openstreetmap.org/search?q=${findDistrictQuery}&format=json&limit=1&addressdetails=4`
   );
 
-  const { display_name, lat, lon, address} = (await response.json())[0];
+  const {display_name, lat, lon, address} = (await response.json())[0];
  
-  const district = address.borough!=undefined? address.borough:"";
-  return  district;
+  return display_name;
     };
- findDistrict().then(district => {
-    // got value district
-    console.log(district);
+ findDistrict().then(display_name => {
+    // got value display_name
+    console.log(display_name);
 
   output.innerHTML =
         "<div><b>Адрес доставки : </b>" + display_name + ". <br /> Растояние <i class='fas fa-road'></i> : " +
