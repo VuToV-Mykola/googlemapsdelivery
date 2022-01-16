4b865d9464fd041b15d7c7976ff0c7ad7a167e9c
VuToV-Mykola <61313070+VuToV-Mykola@users.noreply.github.com>
2022-01-16 18:51:54 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -160,7 +160,7 @@ function calcRoute() {
 
         console.log(query);
         const response = await fetch(
-          `https://nominatim.openstreetmap.org/search?q=${query}&format=json&limit=1&addressdetails=4&countrycodes=UA&accept-language=uk-UA`
+          `https://nominatim.openstreetmap.org/search?q=${query}&format=json&limit=1&addressdetails=4&countrycodes=UA`
         );
 
         const { display_name, lat, lon, address } = (await response.json())[0];
