79879d2e9ccfee003b32905c42d136b1990ae8c5
VuToV-Mykola <61313070+VuToV-Mykola@users.noreply.github.com>
2022-01-16 12:17:29 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -231,10 +231,9 @@ function calcRoute() {
         });
       //display route
       console.log(result);
-
+      map.setCenter(results [0].geometry.location);
       directionsDisplay.setDirections(result);
-
-centerMap( map );
+      
 
     } else {
       //delete route from map
