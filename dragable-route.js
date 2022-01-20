c8bedd80810b2d10a3baa7c6b952b27267a362df
VuToV-Mykola <61313070+VuToV-Mykola@users.noreply.github.com>
2022-01-20 20:34:06 +0200

    Update dragable-route.js

--- dragable-route.js
+++ dragable-route.js
@@ -231,7 +231,7 @@ function plotDirections(start, end) {
       
         var stepIW = new google.maps.InfoWindow();
         stepIW.setPosition(center);
-        stepIW.setContent(`<body style="color:red; background:yellow;"><img src="./Images/directions_car_grey800_24dp.png" alt="авто"><b style="color:black;>`
+        stepIW.setContent(`<body style="width:200px;height:200px;color:red; background:yellow;"><img src="./Images/directions_car_grey800_24dp.png" alt="авто"><b style="color:black;>`
                           + response.routes[i].legs[0].duration_in_traffic.text
                           + `</b><br/><b>` + response.routes[i].legs[0].distance.text+`</b></body>`);
         stepIW.open(directionsDisplay.map);
